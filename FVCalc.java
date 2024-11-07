274bb43ca5f0cb78ff8ee3106dd12b72e44a80f9
Sheli Fine <sheli.fine@post.runi.ac.il>
2024-11-07 07:59:20 +0200

    added a

--- FVCalc.java
+++ FVCalc.java
@@ -8,6 +8,6 @@ public class FVCalc {
 
 		double futureValue = (currentValue * Math.pow(1 + (rate / 100), years));
 
-		System.out.println("After " + years + " years, $" + currentValue + " saved at " + rate + "% will yield $" + (int) futureValue);
+		System.out.println("After " + years + " years, a $" + currentValue + " saved at " + rate + "% will yield $" + (int) futureValue);
 	}
 }
